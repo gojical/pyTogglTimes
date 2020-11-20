6f82442eb238def106a83aaa31e26a4d74697701
Callum Fleming <howzitcallum@gmail.com>
2020-11-20 20:51:35 +0200

    more cleanup

--- process.py
+++ process.py
@@ -15,23 +15,23 @@ def time_to_sec(time_yup):
     seconds += time_yup[2]
     return seconds
 
-
 api_url = "https://api.track.toggl.com/api/v8/time_entries"
 
 for entry in toggl_input:
     r = post(
         api_url,
         auth=(config["TOGGL_API"]["key"], "api_token"),
-        json={"time_entry": {
-            "description": entry['desc'],
-            "created_with": "pyTogglTimes",
-            "start": f"{entry['start']}T{entry['from']}:00+02:00",
-            "duration": time_to_sec(entry['dur']),
-            "billable": entry["billable"],
-            "pid": entry["project"].value,
-            "wid": 331781
-
-        }
+        json={
+            "time_entry": 
+            {
+                "description": entry['desc'],
+                "created_with": "pyTogglTimes",
+                "start": f"{entry['start']}T{entry['from']}:00+02:00",
+                "duration": time_to_sec(entry['dur']),
+                "billable": entry["billable"],
+                "pid": entry["project"].value,
+                "wid": config["TOGGL_API"]["wid"]
+            }
         }
     )
     print("---------------------------------------")
